c192921b7e5c4bc5a5f00dffef5a9e92c7fc12f8
Gil <gilwein@gmail.com>
2018-03-17 12:21:48 -0700

    added JS functionality that checks if the player chose equal cards

--- js/main.js
+++ js/main.js
@@ -1,6 +1,19 @@
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
+var cards = ["queen", "queen", "king", "king"];
+var cardsInPlay = [];
 
-console.log("User flipped " + cardThree + " and " + cardOne);
+var cardOne = cards[0];
+var cardTwo = cards[2];
+
+cardsInPlay.push(cardOne);
+cardsInPlay.push(cardTwo);
+
+if (cardsInPlay.length === 2){
+	if (cardsInPlay[0] === cardsInPlay[1]){
+		alert("You found a match!");
+	} else {
+		alert("Sorry, try again.");
+	}
+}
+
+
+console.log("User flipped " + cardsInPlay);
